8e5778177300da15ee4b024c7b406abba95875fb
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-18 20:34:17 +0100

    ft_strnstr

--- 1-libft/ft_strnstr.c
+++ 1-libft/ft_strnstr.c
@@ -6,32 +6,35 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 15:53:32 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/17 16:33:49 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/18 20:29:56 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char*	ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	if (*little)
-		return ((char *)big);
-	while (big)
-	{
-		int index;
+	size_t index;
 
+	index = 0;
+	if (!(*little))
+		return ((char *)big);
+	while (big && len > 0 && index < len)
+	{
 		index = 0;
-		while (big && *big != *little)
+		while (big && *big != *little && len > 0)
 		{
 			big++;
 			len--;
 		}
-		while (little[index] != '\0' && big[index] == little[index] && index < len)
+		while (little[index] && big[index] == little[index] && index < len)
 		{
 			index++;
 		}
 		if (little[index] == '\0')
 			return ((char *)big);
+		big++;
+		len--;
 	}
 	return (0);
 }
