31dd29ab9c5ece45485461ab2c9c4c6dc6549158
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-19 21:45:13 +0100

    isalnum

--- 1-libft/libft.h
+++ 1-libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 23:18:35 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/19 21:17:34 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/19 21:42:01 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,11 @@ size_t			ft_strlcat(char *dst, const char *src, size_t size);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+int				ft_isalnum(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 char			*ft_strrchr(const char *s, int c);
+char			*ft_strchr(const char *s, int c);
 
 #endif
--- 1-libft/test.c
+++ 1-libft/test.c
@@ -2,15 +2,19 @@
 
 int main(void)
 {
-	char dst[] = "haha";
-	char src[] = "";
+	//char dst[] = "haha";
+	//char src[] = "";
 
-	printf("%lu - %s\n", ft_strlcat(dst, src, 7), dst);
-	printf("%lu - %s\n", ft_strlcpy(dst, src, 7), dst);
+	printf("%d\n", ft_isalnum('5'));
+	printf("%d\n", ft_isalpha('5'));
+	printf("%d\n", ft_isdigit('5'));
+	//printf("%lu - %s\n", ft_strlcat(dst, src, 7), dst);
+	//printf("%lu - %s\n", ft_strlcpy(dst, src, 7), dst);
 	//printf("%lu\n", ft_strlen("Une phrase"));
 	//printf("%d\n", ft_strncmp("aaaa", "aaba", 3));
 	//printf("%s\n", ft_strnstr("Une phrase aleatoire", "rase", 12));
 	//printf("%s\n", ft_strrchr("une phrasekifinipa", (int)'e'));
+	//printf("%s\n", ft_strchr("une phrasekifinipa", (int)'e'));
 	//printf("%c - %c\n", ft_tolower((int)'A'), ft_toupper((int)'a'));
 	return 0;
 }
