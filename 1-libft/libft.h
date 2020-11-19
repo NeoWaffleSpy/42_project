11a2b3ea304f643dd4aa3c94692e6eb24ae6a836
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-19 21:50:41 +0100

    isalnum

--- 1-libft/libft.h
+++ 1-libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 23:18:35 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/19 21:42:01 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/19 21:49:28 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ size_t			ft_strlcat(char *dst, const char *src, size_t size);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
 int				ft_isalnum(int c);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
--- 1-libft/test.c
+++ 1-libft/test.c
@@ -5,9 +5,9 @@ int main(void)
 	//char dst[] = "haha";
 	//char src[] = "";
 
-	printf("%d\n", ft_isalnum('5'));
-	printf("%d\n", ft_isalpha('5'));
-	printf("%d\n", ft_isdigit('5'));
+	//printf("%d\n", ft_isalnum('5'));
+	//printf("%d\n", ft_isalpha('5'));
+	//printf("%d\n", ft_isdigit('5'));
 	//printf("%lu - %s\n", ft_strlcat(dst, src, 7), dst);
 	//printf("%lu - %s\n", ft_strlcpy(dst, src, 7), dst);
 	//printf("%lu\n", ft_strlen("Une phrase"));
