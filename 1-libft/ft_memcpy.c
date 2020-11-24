8d8eaa0b7bba27377e770e2432deef60115207eb
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-24 16:09:02 +0100

    correc

--- 1-libft/ft_memcpy.c
+++ 1-libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 22:55:15 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/24 15:59:20 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/24 16:08:40 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
+	size_t	i;
+	void	*segf;
 
 	if (!dst || !src)
-		return (dst);
+		*((char*)segf) = ((char*)src)[i];
 	i = 0;
 	while (i < n)
 	{
