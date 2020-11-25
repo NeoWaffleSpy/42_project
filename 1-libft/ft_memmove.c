ed5ce621534365f91c66e1a56f57c3f0d4b0f743
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-25 18:00:46 +0100

    correc

--- 1-libft/ft_memmove.c
+++ 1-libft/ft_memmove.c
@@ -6,13 +6,13 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 22:59:08 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/20 23:02:54 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/25 18:00:15 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t n)
+void	*ft_memmove2(void *dst, const void *src, size_t n)
 {
 	unsigned char	tmp[n];
 
@@ -20,3 +20,32 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 	ft_memcpy(dst, tmp, n);
 	return (dst);
 }
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t			i;
+	unsigned char	*a;
+	unsigned char	*b;
+
+	i = 0;
+	a = (unsigned char *)dst;
+	b = (unsigned char *)src;
+	if (a > b)
+	{
+		while (i < len)
+		{
+			a[len - 1] = b[len - 1];
+			len--;
+		}
+	}
+	else
+	{
+		while (i < len)
+		{
+			a[i] = b[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+
