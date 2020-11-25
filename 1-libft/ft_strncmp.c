7ad6c5122b751d8d095cfe9cb76cc04e462df19f
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-26 00:34:34 +0100

    correc

--- 1-libft/ft_strncmp.c
+++ 1-libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 13:23:07 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/22 21:22:53 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/26 00:34:17 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,12 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	index;
-	int		compare;
 
 	index = 0;
-	while (s1 && s2 && s1[index] && s2[index] && index < n - 1)
-	{
-		compare = s1[index] - s2[index];
-		if (compare != 0)
-			return (compare);
+	while ((unsigned char)s1[index] == (unsigned char)s2[index] &&
+			s1[index] && s2[index] && index < n - 1)
 		index++;
-	}
-	compare = s1[index] - s2[index];
-	if (compare != 0)
-		return (compare);
+	if (index < n)
+		return ((unsigned char)s1[index] - (unsigned char)s2[index]);
 	return (0);
 }
