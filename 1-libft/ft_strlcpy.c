045058776ecd964337364314775859d8947859eb
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-26 00:49:41 +0100

    correc

--- 1-libft/ft_strlcpy.c
+++ 1-libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 13:49:51 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/19 21:23:57 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/26 00:49:27 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t index;
 
-	index = 0;
-	if (size == 0)
-		return (0);
-	while (*src && src[index] && index < size)
+	index = ft_strlen(src);
+	if (index + 1 < size)
+		ft_memcpy(dst, src, index + 1);
+	else if (size != 0)
 	{
-		dst[index] = src[index];
-		index++;
+		ft_memcpy(dst, src, size - 1);
+		dst[size - 1] = '\0';
 	}
-	dst[index] = '\0';
 	return (index);
 }
