6c8355d4ac1bb6c0463650e0d63645bd318a39b0
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-25 23:32:05 +0100

    correc

--- 1-libft/ft_memchr.c
+++ 1-libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 23:20:17 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/22 02:19:46 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/25 23:31:39 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	index;
-	void	*tmp;
+	char	*tmp;
 
 	index = 0;
 	tmp = NULL;
 	while (index < n)
 	{
 		if (*((char*)s) == (char)c)
-			tmp = (void*)s;
+			return ((void*)s);
 		s++;
 		index++;
 	}
-	return (tmp);
+	return (NULL);
 }
