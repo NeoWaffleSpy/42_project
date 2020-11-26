446fbd68bb4fad8e6911515fcf25f6d72f97ab98
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-26 01:02:25 +0100

    correc

--- 1-libft/ft_strlcat.c
+++ 1-libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 21:08:33 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/26 00:54:16 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/26 01:02:18 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,19 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t index;
+	size_t index2;
 
 	index = 0;
+	index2 = 0;
 	if (size == 0)
 		return (0);
 	while (dst[index])
 		index++;
-	while (*src && index < size)
-	{
-		dst[index] = *src;
-		index++;
-		src++;
-	}
-	dst[index] = '\0';
-	return (index);
+	while (src[index2] && index < size - 1)
+		dst[index++] = src[index2++];
+	if (size != 0 && size <= ft_strlen(dst))
+		dst[index] = '\0';
+	if (size > ft_strlen(dst))
+		return (ft_strlen(src) + size);
+	return (ft_strlen(src) + ft_strlen(dst));
 }
