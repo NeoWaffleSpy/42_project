8d968d040f48d48550cb91de1eee952aed98abe7
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-27 01:59:58 +0100

    correc

--- 1-libft/ft_substr.c
+++ 1-libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 15:55:12 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/24 02:46:37 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/27 01:59:42 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	if (!s || (start > ft_strlen(s)))
 		return (NULL);
 	new_str = (char*)malloc(sizeof(char) * len + 1);
+	if (!new_str)
+		return (NULL);
 	index = 0;
 	while (index < len && s[start + index])
 	{
