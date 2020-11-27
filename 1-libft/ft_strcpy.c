d8c4d2b47e944c167c832feeb0512096089582da
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-27 22:26:32 +0100

    correc

--- 1-libft/ft_strcpy.c
+++ 1-libft/ft_strcpy.c
@@ -6,24 +6,19 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 16:17:12 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/21 16:24:30 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/27 22:24:12 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strcpy(char *dst, const char *src)
+char	*ft_strcpy(char *dst, const char *src)
 {
 	size_t	index;
 
-	index = 0;
+	index = ft_strlen(src);
 	if (!src)
 		return (NULL);
-	while (src[index])
-	{
-		dst[index] = src[index];
-		index++;
-	}
-	dst[index] = '\0';
-	return (index);
+	ft_memcpy(dst, src, index + 1);
+	return (dst);
 }
