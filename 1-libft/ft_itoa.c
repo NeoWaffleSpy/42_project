92f5dc9cbcdd3937231ba0c1b099d464689a864e
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-27 03:01:34 +0100

    correc

--- 1-libft/ft_itoa.c
+++ 1-libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 21:24:02 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/27 02:45:09 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/27 03:01:25 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char	*return_0(char *s)
 	return (s);
 }
 
-char	*ft_itoa(int chiffre)
+char	*ft_itoa2(int chiffre)
 {
 	char	*str;
 	int		len;
@@ -62,4 +62,59 @@ char	*ft_itoa(int chiffre)
 	}
 	str[string_len] = '\0';
 	return (str);
+}
+
+static int        taille_str(long long int n)
+{
+    long long int    i;
+
+    i = 1;
+    if (n < 0)
+        n = n * -1;
+    while (n >= 10)
+    {
+        n = n / 10;
+        i++;
+    }
+    return (i);
+}
+
+static char        *rempli_str(char *str, long long int n, int len)
+{
+    int        debut;
+
+    str[len] = '\0';
+    len = len - 1;
+    if (n < 0)
+    {
+        n = n * -1;
+        debut = 1;
+        str[0] = '-';
+    }
+    else
+        debut = 0;
+    while (len >= debut)
+    {
+        str[len] = n % 10 + '0';
+        n = n / 10;
+        len--;
+    }
+    return (str);
+}
+
+char            *ft_itoa(int n)
+{
+    char            *str;
+    int                len;
+    long long int    lln;
+
+    lln = n;
+    len = taille_str(lln);
+    if (lln < 0)
+        len = len + 1;
+    str = ft_strnew(len);
+    if (!str)
+        return (NULL);
+    str = rempli_str(str, lln, len);
+    return (str);
 }
