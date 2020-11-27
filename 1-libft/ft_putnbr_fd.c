7aef331073a0e7c89daa58de0427f6b043d33919
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-27 21:45:09 +0100

    correc

--- 1-libft/ft_putnbr_fd.c
+++ 1-libft/ft_putnbr_fd.c
@@ -6,21 +6,11 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 01:24:45 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/27 21:41:33 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/27 21:44:27 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
-
-void	ft_putnbr_fd2(int n, int fd)
-{
-	char *s;
-
-	s = ft_itoa(n);
-	ft_putstr_fd(s, fd);
-	free(s);
-}
 
 void	ft_putnbr_fd(int n, int fd)
 {
