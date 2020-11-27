a140f027641da0f4239e30ff5369f5bc7db3ff2e
Nathan <nathan.caba@etu.u-pec.fr>
2020-11-27 02:26:59 +0100

    correc

--- 1-libft/ft_strjoin.c
+++ 1-libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ncaba <nathancaba.etu@outlook.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 16:08:33 by ncaba             #+#    #+#             */
-/*   Updated: 2020/11/24 02:18:45 by ncaba            ###   ########.fr       */
+/*   Updated: 2020/11/27 02:26:48 by ncaba            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ char	*ft_strjoin(const char *s1, const char *s2)
 		return (ft_strdup(s1));
 	len = ft_strlen(s1) + ft_strlen(s2);
 	new_str = (char*)malloc(sizeof(char) * len + 1);
+	if (!new_str)
+		return (NULL);
 	ft_strcpy(new_str, s1);
 	ft_strcat(new_str, s2);
 	return (new_str);
