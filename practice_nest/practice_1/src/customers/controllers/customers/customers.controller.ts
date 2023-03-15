dcec1853814fe09b7b264045836f9c74302e4929
NeoWaffleSpy <nathancaba.etu@outlook.fr>
2023-03-15 17:35:50 +0100

    practice

--- practice_nest/practice_1/src/customers/controllers/customers/customers.controller.ts
+++ practice_nest/practice_1/src/customers/controllers/customers/customers.controller.ts
@@ -1,13 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CustomersService } from 'src/customers/services/customers/customers.service';
 
 @Controller('customers')
 export class CustomersController {
+	constructor(private customersService: CustomersService) {}
 	@Get('')
 	getCustomer() {
-		return {
-			id: 1,
-			email: 'ncaba@42.fr',
-			createdAt: new Date(),
-		};
+		return this.customersService.findCustomer();
 	}
 }
